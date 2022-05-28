0e4b17b4b0ad4cf23262bc37641ed6b9b3df0d49
Mikhail Laguzov <m-laguzov@yandex.ru>
2022-05-28 10:06:50 +0300

    Сверстать popup

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-let like = document.querySelectorAll('.element__like');
-console.log(like);
+const editButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
+const popupClose = document.querySelector('.popup__close')
+const profileName = document.querySelector('.profile__full-name')
+const profileJob = document.querySelector('.profile__job')
+const popupInputName = document.querySelector('.popup__input_name')
+const popupInputJob = document.querySelector('.popup__input_job')
+const popupSubmit = document.querySelector('.popup__submit')
 
-function showClick() {
-    alert('Вы кликнули по элементу');
-}
 
-like.addEventListener('click', showClick());
+editButton.addEventListener('click', function() {
+    popup.classList.add('popup_open')
+    popupInputName.value = profileName.textContent
+    popupInputJob.value = profileJob.textContent
+})
+
+popupClose.addEventListener('click', function() {
+    popup.classList.remove('popup_open')
+    profileName.textContent = popupInputName.value
+    profileJob.textContent = popupInputJob.value
+})
+
+popupSubmit.addEventListener('click', function() {
+    popup.classList.remove('popup_open')
+    profileName.textContent = popupInputName.value
+    profileJob.textContent = popupInputJob.value
+})
