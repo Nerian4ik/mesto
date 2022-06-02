339dcfd921f5ed55b128df9474c2bc1161f955f8
Mikhail Laguzov <m-laguzov@yandex.ru>
2022-06-02 20:25:36 +0300

    Исправление ошибок после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,31 @@
-const editButton = document.querySelector('.profile__edit-button')
-const popup = document.querySelector('.popup')
-const popupClose = document.querySelector('.popup__close')
-const profileName = document.querySelector('.profile__full-name')
-const profileJob = document.querySelector('.profile__job')
-const popupInputName = document.querySelector('.popup__input_type_name')
-const popupInputJob = document.querySelector('.popup__input_type_job')
-const popupSubmit = document.querySelector('.popup__submit')
-const form = document.querySelector('.popup__form')
+const editButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+const profileName = document.querySelector('.profile__full-name');
+const profileJob = document.querySelector('.profile__job');
+const popupInputName = document.querySelector('.popup__input_type_name');
+const popupInputJob = document.querySelector('.popup__input_type_job');
+const popupSubmit = document.querySelector('.popup__submit');
+const form = document.querySelector('.popup__form');
 
-function open() {
-    popup.classList.remove('popup_open')
-    profileName.textContent = popupInputName.value
-    profileJob.textContent = popupInputJob.value
+function close() {
+    popup.classList.remove('popup_open');
 }
 
 editButton.addEventListener('click', function() {
-    popup.classList.add('popup_open')
-    popupInputName.value = profileName.textContent
-    popupInputJob.value = profileJob.textContent
+    popup.classList.add('popup_open');
+    popupInputName.value = profileName.textContent;
+    popupInputJob.value = profileJob.textContent;
 })
 
-popupClose.addEventListener('click', open)
+popupClose.addEventListener('click', close);
 
 
 function formSubmitHandler(evt) {
-    evt.preventDefault()
-    profileName.textContent = popupInputName.value
-    profileJob.textContent = popupInputJob.value
-    open()
+    evt.preventDefault();
+    profileName.textContent = popupInputName.value;
+    profileJob.textContent = popupInputJob.value;
+    close();
 }
 
-form.addEventListener('submit', formSubmitHandler)
+form.addEventListener('submit', formSubmitHandler);
